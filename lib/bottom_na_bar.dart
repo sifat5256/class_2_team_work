7772d11c87ae785275541d7e83206babce375077
IQRAMUL HASAN SIFAT <140641867+sifat5256@users.noreply.github.com>
2025-02-23 11:19:51 +0600

    add_bottom_nav_bar

---
+++ lib/bottom_na_bar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+
+class BottomNavBarScreen extends StatefulWidget {
+  @override
+  _BottomNavBarScreenState createState() => _BottomNavBarScreenState();
+}
+
+class _BottomNavBarScreenState extends State<BottomNavBarScreen> {
+  int _selectedIndex = 0;
+
+  // List of pages
+  final List<Widget> _pages = [
+    Center(child: Text('Home Screen', style: TextStyle(fontSize: 20))),
+    Center(child: Text('Search Screen', style: TextStyle(fontSize: 20))),
+    Center(child: Text('Profile Screen', style: TextStyle(fontSize: 20))),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.list_alt_outlined), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'bottom_na_bar.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,6 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -30,7 +34,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:  BottomNavBarScreen(),
     );
   }
 }
